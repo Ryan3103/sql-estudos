b8925db5eed9a01bf16f2e28161e6ba360a38d4a
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-15 16:20:05 -0300

    Adiciona exercício 02 de consultas SQL

---
+++ 01-basico/exercicio02.sql
@@ -0,0 +1,11 @@
+SELECT * FROM produtos;
+
+SELECT nome, preco FROM produtos;
+
+SELECT * FROM produtos WHERE preco > 20;
+
+SELECT * FROM produtos WHERE preco < 30;
+
+SELECT * FROM produtos WHERE preco = 20;
+
+SELECT * FROM produtos WHERE preco >= 29.90;
